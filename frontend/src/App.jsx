6269bc4b8df56ce3a08ac5f39fc7929b60450edb
Raelton24 <israeluyi7@gmail.com>
2025-03-15 15:44:41 +0100

    Integrated frontend into main repository

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import {Box} from "@chakra-ui/react";
+import { Route, Routes } from "react-router-dom";
+import CreatePage from "./pages/CreatePage";
+import HomePage from "./pages/HomePage";
+import Navbar from "./components/Navbar";
+import { useColorModeValue } from "./components/ui/color-mode";
+
+import { Toaster } from "@/components/ui/toaster";
+
+function App() {
+  return (
+    
+    <Box minH={"100vh"} bg={useColorModeValue("#F8F8F8", "blue.900")}>
+      <Toaster />
+      <Navbar />
+      <Routes>
+        
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/create" element={<CreatePage/>} />
+      </Routes>
+    </Box>
+  );
+}
+
+export default App
